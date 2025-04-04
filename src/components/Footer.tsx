95aea17a0139d729be51adff500e98a4ee3cecae
chauanphu <chauanphu@gmail.com>
2025-04-04 04:13:20 +0000

    update name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto px-4 text-center">
-        <p>© {currentYear} CTY TNHH Gia Phát | Phiên bản v3.1.0</p>
+        <p>© {currentYear} CTY TNHH Gia Phát | Hệ thống SCADA CGP | Phiên bản v3.1.0</p>
       </div>
     </footer>
   );
